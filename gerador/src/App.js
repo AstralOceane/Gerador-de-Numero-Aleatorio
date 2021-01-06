dd3118997454aeb213ead77a0d4d0788c0875db0
Matheus <matheus.novaes998@gmail.com>
2021-01-06 14:45:09 -0300

    pronto

--- gerador/src/App.js
+++ gerador/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  
+`
+const FolhaContainer = styled.div`
+  height: 400px;
+  width: 600px;
+  box-shadow: 10px 10px 5px grey;
+`
 
 function App() {
+  const [numeroAleatorio, setNumeroAleatorio ] = useState (0) 
+  const GerarNumero = () =>{
+      const Armazem = Math.floor(Math.random() * (100 - 1) + 1)
+      setNumeroAleatorio (Armazem)
+  }
   return (
-    <div className="App">
+   <AppContainer>
+        <FolhaContainer>
+            <h2> 
+               Numero Aleatorio
+            </h2>
+            <h1>
+              {numeroAleatorio}
+            </h1>
 
-    </div>
+            <p>
+              Click no botao abaixo para gerar um numero aleatorio
+            </p>
+            <button onClick={GerarNumero}>Gerar</button>
+        </FolhaContainer>   
+   </AppContainer>
   );
 }
+
 
 export default App;
